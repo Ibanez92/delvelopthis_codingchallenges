f650e00f679b8bcba8e5dddbf45dc6a54e89d841
Javier Ibanez <Expertprep2015@gmail.com>
2023-03-07 23:16:49 -0500

    added solution to blackjack.js prompt

--- 05/blackjack/blackjack.js
+++ 05/blackjack/blackjack.js
@@ -1,11 +1,7 @@
 const doIHit = (card_1, card_2) => {
     // hit anytime the card total is 14 or less
-    cardTotal = Number(card_1) + Number(card_2)
-    if (cardTotal <= 14) {
-        return console.log(1);
-    } else {
-        return console.log(0);
-    }
+    let cards = {"2":2, "3":3, "4": 4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K":10, "A":11}
+    return cards[card_1] + cards[card_2] > 14 ? 0 : 1
 }
 
 module.exports = doIHit;
