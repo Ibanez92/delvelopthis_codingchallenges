4c5c07e84dd685f94e27824c348d938210c52da8
Javier Ibanez <Expertprep2015@gmail.com>
2023-03-21 22:00:48 -0400

    worked on problems in folder 07

--- 07/palindrome/palindrome-unsolved.js
+++ 07/palindrome/palindrome-unsolved.js
@@ -1,6 +1,11 @@
 function isPalindrome(str) {
 
-  }
+  const strArray = str.split("")
+  if(strArray.join("") === strArray.reverse().join("")){
+    return 'true'
+  } else return 'false'
+
+}
 
 console.log(isPalindrome('A man, a plan, a canal: Panama'))
 
