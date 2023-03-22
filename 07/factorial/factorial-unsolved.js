cebc9a2f2ed88a91e4cb988e6c81d4adeceeada7
Javier Ibanez <Expertprep2015@gmail.com>
2023-03-21 22:02:57 -0400

    worked on the problems in folder 07

--- 07/factorial/factorial-unsolved.js
+++ 07/factorial/factorial-unsolved.js
@@ -1,4 +1,11 @@
-function factorial(num) {}
+function factorial(num) {
+    let product = 1
+    if(num > 0){
+        if(num == 1) return 1;
+        return num*factorial(num-1)
+    }
+    return 'no negative ints'
+}
 
 console.log(factorial(4));
 
--- 07/fizz-buzz/fizz-buzz-unsolved.js
+++ 07/fizz-buzz/fizz-buzz-unsolved.js
@@ -1,5 +1,15 @@
 function fizzBuzz(num) {
-
+    const array = [] 
+    for (let i = 1; i <= num; i++) {
+        if(i % 15 ===0){
+            array.push("FizzBuzz")
+        } else if (i % 3 === 0){
+            array.push("Fizz")
+        } else if (i % 5 === 0){
+            array.push("Buzz")
+        } else array.push(i.toString())
+    }
+    return array
 }
 
 console.log(fizzBuzz(15));
