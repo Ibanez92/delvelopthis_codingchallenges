47a01400b260d0ad72ba3ee9fcfa27b72622e14d
Javier Ibanez <Expertprep2015@gmail.com>
2023-03-26 23:52:15 -0400

    adding comments explaining my code for fizzbuzz problem in folder 07

--- 07/fizz-buzz/fizz-buzz-unsolved.js
+++ 07/fizz-buzz/fizz-buzz-unsolved.js
@@ -1,6 +1,10 @@
+// created a function called fizzbuzz
 function fizzBuzz(num) {
+    // created an empty array for my answer to go into 
     const array = [] 
+    // looped through the number input starting at 1
     for (let i = 1; i <= num; i++) {
+        // if number is divisable by 15 then push the string 'FizzBuzz' into the array
         if(i % 15 ===0){
             array.push("FizzBuzz")
         } else if (i % 3 === 0){
