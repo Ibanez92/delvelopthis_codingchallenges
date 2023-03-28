6b508e6fbd2f92c85ac63a2f185d2f0839a55cee
Javier Ibanez <Expertprep2015@gmail.com>
2023-03-27 23:49:23 -0400

    started comment explaing my factorial problem folder 07

--- 07/factorial/factorial-unsolved.js
+++ 07/factorial/factorial-unsolved.js
@@ -1,3 +1,4 @@
+// factorial of a number is the sum of multiplication of all the integers smaller than that positive integer. For example, factorial of 5 is 5 * 4 * 3 * 2 * 1 which equals to 120
 function factorial(num) {
     if(num > 0){
         if(num == 1) return 1;
