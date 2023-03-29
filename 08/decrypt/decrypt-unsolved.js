678a07dc805e745ff786eee168a3063f705bc276
Javier Ibanez <Expertprep2015@gmail.com>
2023-03-28 21:25:59 -0400

    finished decrypt-unsolved problem in folder 08

--- 08/decrypt/decrypt-unsolved.js
+++ 08/decrypt/decrypt-unsolved.js
@@ -1,6 +1,16 @@
 function decrypt(cipher, key) {
     let decrypted = ""
 
+    for (let i = 0; i < cipher.length; i++) {
+        const thisLetter = cipher[i];
+        if(cipher.charCodeAt(i) >= 65 && cipher.charCodeAt(i) <= 90 || cipher.charCodeAt(i) <= 122){
+            let newLetter = cipher.charCodeAt(i) - key
+
+            decrypted += String.fromCharCode(newLetter)
+        } else {
+            decrypted += thisLetter
+        }
+    }
     return decrypted
 }
 
