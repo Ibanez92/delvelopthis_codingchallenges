1826c20f25d55e84b3c21d1ac2df471c05bb728d
Javier Ibanez <Expertprep2015@gmail.com>
2023-03-29 23:56:51 -0400

    added comments explaining code in decrypt-unsolved.js file

--- 08/decrypt/decrypt-unsolved.js
+++ 08/decrypt/decrypt-unsolved.js
@@ -3,9 +3,13 @@ function decrypt(cipher, key) {
     // created decrypted variable to hold my decrypted string
     let decrypted = ""
 
+    // loop through the cipher string
     for (let i = 0; i < cipher.length; i++) {
+        // letter on the cipher string
         const thisLetter = cipher[i];
+        // if cipher string is greater than the character code at(ascii) 90 or cipher string is less than or equal to 122
         if(cipher.charCodeAt(i) >= 65 && cipher.charCodeAt(i) <= 90 || cipher.charCodeAt(i) <= 122){
+            // create newLetter variable holding the cipher string minus the key
             let newLetter = cipher.charCodeAt(i) - key
 
             decrypted += String.fromCharCode(newLetter)
