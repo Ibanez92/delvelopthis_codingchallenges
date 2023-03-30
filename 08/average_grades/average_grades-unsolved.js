854ad37cdb98b325450dd65bb1ab1773f17150af
Javier Ibanez <Expertprep2015@gmail.com>
2023-03-29 23:58:47 -0400

    formatted document on average_grade-unsolved file in folder 08

--- 08/average_grades/average_grades-unsolved.js
+++ 08/average_grades/average_grades-unsolved.js
@@ -1,34 +1,50 @@
-function averageGrades(grades) {
-  // const scoreMap = new Map() 
-  const scoreMap = {};
+function averageGrades(grades) { // const scoreMap = new Map()
+    const scoreMap = {};
 
-  for (let i = 0; i < grades.length; i++) {
-    const thisGrade = grades[i]
-    if(scoreMap[thisGrade.name]){
-      scoreMap[thisGrade.name].push(thisGrade.grade)
-    } else {
-      scoreMap[thisGrade.name] = [thisGrade.grade]
+    for (let i = 0; i < grades.length; i++) {
+        const thisGrade = grades[i]
+        if (scoreMap[thisGrade.name]) {
+            scoreMap[thisGrade.name].push(thisGrade.grade)
+        } else {
+            scoreMap[thisGrade.name] = [thisGrade.grade]
+        }
     }
-  }
-  // console.log(scoreMap);
-  for(const name in scoreMap){
-    const grades = scoreMap[name];
-    const average = grades.reduce((a, b) => a + b) / grades.length;
-    scoreMap[name] = average;
-  }
-  console.log(scoreMap);
+    // console.log(scoreMap);
+    for (const name in scoreMap) {
+        const grades = scoreMap[name];
+        const average = grades.reduce((a, b) => a + b) / grades.length;
+        scoreMap[name] = average;
+    }
+    console.log(scoreMap);
 }
 
 const grades = [
-  { name: "Alice", grade: 75 },
-  { name: "Bob", grade: 80 },
-  { name: "Alice", grade: 85 },
-  { name: "Bob", grade: 90 },
-  { name: "Charlie", grade: 70 },
-  { name: "Alice", grade: 90 },
-  { name: "Charlie", grade: 80 },
+    {
+        name: "Alice",
+        grade: 75
+    },
+    {
+        name: "Bob",
+        grade: 80
+    },
+    {
+        name: "Alice",
+        grade: 85
+    },
+    {
+        name: "Bob",
+        grade: 90
+    }, {
+        name: "Charlie",
+        grade: 70
+    }, {
+        name: "Alice",
+        grade: 90
+    }, {
+        name: "Charlie",
+        grade: 80
+    },
 ];
-
 
 
 console.log(averageGrades(grades));
