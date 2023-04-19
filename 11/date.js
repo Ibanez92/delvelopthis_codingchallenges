026231d85cca0ebb7724a22d472bdfd21879d28f
Javier Ibanez <Expertprep2015@gmail.com>
2023-04-18 21:03:47 -0400

    added new folder for week 11 of develop this coding problems

---
+++ 11/date.js
@@ -0,0 +1,11 @@
+const date = new Date();
+
+const dayOfWeek = (date.toLocaleString(undefined, {weekday: 'long'}))
+const time = date.toLocaleTimeString((undefined, {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+}));
+
+console.log(`Today is : ${dayOfWeek}`)
+console.log(`Current time is : ${time} : ${minSec}`);
