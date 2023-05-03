e6ea835b0926e0c20e9c59e5c8b973ef604ed0e6
Javier Ibanez <Expertprep2015@gmail.com>
2023-05-02 20:28:45 -0400

    finished number 16 of the w3 schools javacript coding problems

--- 13/script.js
+++ 13/script.js
@@ -1,2 +1,14 @@
-const varName = prompt('what is your name?')
-alert(`You entered ${varName}`)
+const myForm = document.getElementById('form')
+
+function myFunction(num1, num2) {
+    if(num1 === num2) return (num1 + num2) * 3;
+    else return num1 + num2;
+}
+
+myForm.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    const myFormData = new FormData(myForm)
+    console.log(myFormData.get('num1'));
+    console.log(myFormData.get('num2'));
+
+})
